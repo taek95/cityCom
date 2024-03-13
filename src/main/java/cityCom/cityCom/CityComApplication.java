6b73a7260d76c4ca6ea17652c5e57febe8c1c801
taek95 <01077275247a@gmail.com>
2024-03-13 17:41:31 +0900

    first commit

---
+++ src/main/java/cityCom/cityCom/CityComApplication.java
@@ -0,0 +1,13 @@
+package cityCom.cityCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cityCom/cityCom/CityComApplicationTests.java
@@ -0,0 +1,13 @@
+package cityCom.cityCom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
